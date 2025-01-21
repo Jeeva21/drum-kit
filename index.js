1b7b51a56b424c47798230fc5954f120b043d717
jivatech <jeevakbs@gmail.com>
2025-01-21 10:53:58 +0530

    Add keyboard keypress everntlistener

--- index.js
+++ index.js
@@ -1,45 +1,55 @@
 for(i=0;i<document.querySelectorAll(".drum").length;i++){
     document.querySelectorAll(".drum")[i].addEventListener("click", function(){
         var letter = this.innerHTML;
-        console.log("The letter is: " + letter);
-        switch (letter) {
-            case "w":
-                var audio = new Audio("./sounds/crash.mp3");
-                audio.play();
-                break;
-            
-            case "a":
-                var audio = new Audio("./sounds/kick-bass.mp3");
-                audio.play();
-                break;
-
-            case "s":
-                var audio = new Audio("./sounds/snare.mp3");
-                audio.play();
-                break;
-
-            case "d":
-                var audio = new Audio("./sounds/tom-1.mp3");
-                audio.play();
-                break;
-
-            case "j":
-                var audio = new Audio("./sounds/tom-2.mp3");
-                audio.play();
-                break;
-
-            case "k":
-                var audio = new Audio("./sounds/tom-3.mp3");
-                audio.play();
-                break;
-
-            case "l":
-                var audio = new Audio("./sounds/tom-4.mp3");
-                audio.play();
-                break;
-        
-            default:
-                break;
-        }
+        console.log("The letter is from Click: " + letter);
+        checkLetter(letter);
     });
+}
+
+document.addEventListener("keypress", function(e){
+    console.log(e);
+    console.log(e.key);
+    checkLetter(e.key);
+});
+
+function checkLetter(letter){
+    switch (letter) {
+        case "w":
+            var audio = new Audio("./sounds/crash.mp3");
+            audio.play();
+            break;
+        
+        case "a":
+            var audio = new Audio("./sounds/kick-bass.mp3");
+            audio.play();
+            break;
+
+        case "s":
+            var audio = new Audio("./sounds/snare.mp3");
+            audio.play();
+            break;
+
+        case "d":
+            var audio = new Audio("./sounds/tom-1.mp3");
+            audio.play();
+            break;
+
+        case "j":
+            var audio = new Audio("./sounds/tom-2.mp3");
+            audio.play();
+            break;
+
+        case "k":
+            var audio = new Audio("./sounds/tom-3.mp3");
+            audio.play();
+            break;
+
+        case "l":
+            var audio = new Audio("./sounds/tom-4.mp3");
+            audio.play();
+            break;
+    
+        default:
+            break;
+    }
 }
